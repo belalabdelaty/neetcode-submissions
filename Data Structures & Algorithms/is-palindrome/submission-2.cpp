338dd0cfce2e4530bf83844fe655d02842ea8c31
Belal Ahmed <143216550+belalabdelaty@users.noreply.github.com>
2026-07-06 01:56:38 +0300

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        s.erase(remove(s.begin(), s.end(), ' '), s.end());
+        transform(s.begin(), s.end(), s.begin(), [](unsigned char c) {return tolower(c);});
+        for(int i = 0; i < s.length(); i++){
+            if((s[i] >= 'a' and s[i] <= 'z' ) or (s[i] >= '0' and s[i] <= '9')) continue;
+            else {
+                s.erase(i,1);
+            }
+        }
+        int startt = 0, endd = s.length()-1;
+        while(endd > startt){
+            if((s[startt] < 'a' and s[startt] > 'z' ) and (s[startt] < '0' and s[startt] > '9')) {startt++;}
+            if((s[endd] < 'a' and s[endd] > 'z' ) and (s[endd] < '0' and s[endd] > '9')) {endd--;}
+           
+            if(s[startt]!=s[endd]) {
+                return false;
+            }
+            startt++;
+            endd--;
+        } 
+    return true;
+    }
+};
