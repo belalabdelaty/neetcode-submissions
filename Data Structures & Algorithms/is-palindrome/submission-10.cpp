1e747cc9b4c7209159108e06901f45f473e5b763
Belal Ahmed <143216550+belalabdelaty@users.noreply.github.com>
2026-07-06 02:07:15 +0300

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        s.erase(remove(s.begin(), s.end(), ' '), s.end());
+        transform(s.begin(), s.end(), s.begin(), [](unsigned char c) {return tolower(c);});
+        int startt = 0, endd = s.length()-1;
+        if(startt==endd) return true;
+        while(endd > startt){
+            while((s[startt] < 'a' or s[startt] > 'z' ) and (s[startt] < '0' or s[startt] > '9')) {startt++;}
+            while((s[endd] < 'a' or s[endd] > 'z' ) and (s[endd] < '0' or s[endd] > '9')) {endd--;}
+            if(endd<=startt) break;
+            if(s[startt]!=s[endd]) {
+                return false;
+            }
+            startt++;
+            endd--;
+        } 
+    return true;
+    }
+};
